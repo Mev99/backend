45742136ddc01344fc618be4dd1b5e2ea491af56
Matias Ayesa <matiasayesa99@outlook.com>
2023-07-26 16:06:27 -0300

    second proyect work

--- app.js
+++ app.js
@@ -1,78 +1,88 @@
-class ProductManager {
-    constructor() {
-        this.products = []
-    }
+// class ProductManager {
+//     constructor() {
+//         this.products = []
+//     }
 
-    addProduct(product) {
-        if (this.existingCode(product.code)) {
-            return console.log("This product's code alredy exists")
-        }
+//     addProduct(product) {
+//         if (this.existingCode(product.code)) {
+//             return console.log("This product's code alredy exists")
+//         }
 
-        if (!this.validateProducts(product)) {
-            return console.log("This product is invalid, try again")
-        }
+//         if (!this.validateProducts(product)) {
+//             return console.log("This product is invalid, try again")
+//         }
 
-        product.id = this.randomId()
-        this.products.push(product)
-    }
+//         product.id = this.randomId()
+//         this.products.push(product)
+//     }
 
-    randomId(length = 6) {
-        return Math.random().toString(36).substring(2, length + 2)
-    }
+//     randomId(length = 6) {
+//         return Math.random().toString(36).substring(2, length + 2)
+//     }
 
-    existingCode(code) {
-        let findCode = this.products.find((e) => e.code === code)
-        return findCode ? true : false
-    }
+//     existingCode(code) {
+//         let findCode = this.products.find((e) => e.code === code)
+//         return findCode ? true : false
+//     }
 
-    validateProducts(product) {
-        return (
-            product.title &&
-            product.description &&
-            product.price &&
-            product.thumbnail &&
-            product.code &&
-            product.stock
-            // product.id
-        )
-    }
+//     validateProducts(product) {
+//         return (
+//             product.title &&
+//             product.description &&
+//             product.price &&
+//             product.thumbnail &&
+//             product.code &&
+//             product.stock
+//             // product.id
+//         )
+//     }
 
-    getProducts() {
-        return this.products.forEach(e => console.log(e))
-    }
+//     getProducts() {
+//         return this.products.forEach(e => console.log(e))
+//     }
 
-    getProductById(id) {
-        const productId = this.products.find((e) => e.id === id)
-        if (productId) {
-            return console.log(productId)
-        } else {
-            console.log("that shit wrong")
-        }
-    }
+//     getProductById(id) {
+//         const productId = this.products.find((e) => e.id === id)
+//         if (productId) {
+//             return console.log(productId)
+//         } else {
+//             console.log("that shit wrong")
+//         }
+//     }
+// }
+
+// const managingProduct = new ProductManager
+
+// managingProduct.addProduct({
+//     title: "Mushroom A",
+//     description: "Cool mushroom A",
+//     price: 50,
+//     thumbnail: "./imageOfCoolMushroomA.png",
+//     code: "1",
+//     stock: 5,
+//     // id: 1
+// })
+
+// managingProduct.addProduct({
+//     title: "Mushroom B",
+//     description: "Cool mushroom B",
+//     price: 75,
+//     thumbnail: "./imageOfCoolMushroomB.png",
+//     code: "2",
+//     stock: 8,
+//     // id: 2
+// })
+
+// managingProduct.getProducts()
+
+// // managingProduct.getProductById(1)
+
+function mostrarLetras(string) {
+    let letrita = string
+    setInterval(() => {
+        letrita.length
+    }, 1000);
 }
 
-const managingProduct = new ProductManager
 
-managingProduct.addProduct({
-    title: "Mushroom A",
-    description: "Cool mushroom A",
-    price: 50,
-    thumbnail: "./imageOfCoolMushroomA.png",
-    code: "1",
-    stock: 5,
-    // id: 1
-})
-
-managingProduct.addProduct({
-    title: "Mushroom B",
-    description: "Cool mushroom B",
-    price: 75,
-    thumbnail: "./imageOfCoolMushroomB.png",
-    code: "2",
-    stock: 8,
-    // id: 2
-})
-
-managingProduct.getProducts()
-
-// managingProduct.getProductById(1)
+mostrarLetras('gilDeGoma')
---
+++ segundaEntrega.js
@@ -0,0 +1,101 @@
+const fs = require("fs")
+
+class Container {
+    constructor(file) {
+        this.file = file
+        this.objectArray = []
+        this.id = 1
+    }
+
+    async readTheFile() {
+        try {
+            const data = await fs.promises.readFile(this.file, "utf-8")
+            return data ? JSON.parse(data) : []
+        }
+        catch (error) {
+            console.log("error READING the file", error)
+            return []
+        }
+    }
+
+    async save(object) {
+        try {
+            object.id = this.id++
+
+            const checkProduct = await this.readTheFile()
+            const checkingId = checkProduct.some((e) => { e.title === object.title })
+            console.log(checkingId)
+
+            if (!checkingId) {
+                this.objectArray.push(object)
+                await fs.promises.writeFile(this.file, JSON.stringify(this.objectArray))
+                console.log("object saved correctly", "id:", object.id)
+            } else {
+                console.log("this product alredy exists")
+            }
+
+        } catch (error) {
+            console.log("error SAVING the object: ", error)
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const dataParsed = await this.readTheFile()
+
+            const objectId = await dataParsed.find((e) => e.id === id)
+            if (objectId) {
+                return console.log(objectId)
+            } else {
+                console.log(null)
+            }
+        }
+        catch (error) {
+            console.log("couldn't get the product by it's id: ", error)
+        }
+    }
+
+    async getAll() {
+        try {
+            console.log(await this.readTheFile())
+        }
+        catch (error) {
+            console.log("Did not get em' all", error)
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            const data = await this.readTheFile()
+            const dataFiltered = data.filter((e) => e.id != id)
+            await fs.promises.writeFile(this.file, JSON.stringify(dataFiltered))
+        } catch (error) {
+            console.log("Error deleting an element by ID: ", error)
+        }
+    }
+
+    async deleteAll() {
+        try {
+            await fs.promises.unlink(this.file)
+        }
+        catch (error) {
+            console.log("couldn't erase the file")
+        }
+    }
+}
+
+const container = new Container("products.json")
+
+const qwerty = { title: "title1", price: 50, thumbnail: "url/this/thumbnail.png" }
+const qwerty2 = { title: "title2", price: 100, thumbnail: "url/this/thumbnail.png" }
+
+container.save(qwerty)
+container.save(qwerty2)
+
+// container.getProductById(2)
+
+// container.getAll()
+
+// container.deleteById(2)
+
+// container.deleteAll()
